bd5dbc1229073a62197c71c28749f122345653a3
denisa-iordache <74931542+denisa-iordache@users.noreply.github.com>
2022-02-07 14:00:32 +0200

    Update Footer.js

--- gui/src/components/Footer.js
+++ gui/src/components/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
     return(
         <footer id="footer">
         <p>
-        <a href="https://github.com/denisa-iordache/Proiect_TW_Donald_DAC">GitHub Repository</a></p>
+        <a href="https://github.com/denisa-iordache/Examen_TW">GitHub Repository</a></p>
         </footer>
     )
 }
